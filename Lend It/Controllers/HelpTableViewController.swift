aa06ed04da55045fda9a244a30df03d122225e46
Grecia Escarcega Maldonado <grecia.escarcega@c3.unam.mx>
2019-02-01 04:37:36 -0600

    vista de ayuda sin detalles

--- Lend It/Controllers/HelpTableViewController.swift
+++ Lend It/Controllers/HelpTableViewController.swift
@@ -45,7 +45,34 @@ class HelpTableViewController: UITableViewController {
         return []
     }
     
-    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = HelpViewController()
+        
+        vc.titleLabel!.text = self.helpItems[indexPath.row].title
+        vc.helpTextLabel!.text = self.helpItems[indexPath.row].text
+        
+        performSegue(withIdentifier: "helpSegue", sender: self)
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "helpItemCell", for: indexPath) as! HelpTableViewCell
+
+        cell.helpItemLabel.text = self.helpItems[indexPath.row].title
+
+        return cell
+    }
+
+}
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "helpSegue" {
+//            let vc = segue.destination as! HelpViewController
+//            vc.titleLabel?.text = self.titleSegue
+//            vc.helpTextLabel?.text = self.textSegue
+//        }
+//
+//    }
+
+
 //    func getHelpItems() {
 //
 //        if let path = Bundle.main.path(forResource: "HelpDataSource", ofType: "json") {
@@ -69,59 +96,4 @@ class HelpTableViewController: UITableViewController {
 //        else {debugPrint("not found")}
 //    }
 //
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "helpItemCell", for: indexPath) as! HelpTableViewCell
 
-        cell.helpItemLabel.text = self.helpItems[indexPath.row].title
-
-        return cell
-    }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-   // override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    //}
-
-
-}
---
+++ Lend It/Controllers/HelpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HelpViewController.swift
+//  Lend It
+//
+//  Created by Grecia Escárcega on 2/1/19.
+//  Copyright © 2019 Daniel Esteban Salinas Suárez. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var helpTextLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+}
