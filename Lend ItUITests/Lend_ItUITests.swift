bfbee5f19463b9707a2e5bce9a70983ae070fbcd
Daniel Esteban Salinas Suárez <daniel@Daniels-MacBook-Pro.local>
2019-02-02 17:35:40 -0600

    Add UITests set up, teardown and add item test

--- Lend ItUITests/Lend_ItUITests.swift
+++ Lend ItUITests/Lend_ItUITests.swift
@@ -10,29 +10,62 @@ import XCTest
 
 class Lend_ItUITests: XCTestCase {
 
-    static var app: XCUIApplication!
+    let app = XCUIApplication()
     
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
-        
-        if Lend_ItUITests.app == nil {
-            Lend_ItUITests.app = XCUIApplication()
-            Lend_ItUITests.app.launch()
-            Lend_ItUITests.app.launchArguments.append("--uitesting")
+        app.launch()
+        // Log into the app if nedded before every test
+        if !app.buttons["button lend it"].exists {
+            let facebookSignInButton = app.buttons["facebook"]
+            facebookSignInButton.tap()
+            app.tap()
+            
+            let webView = app.descendants(matching: .webView)
+            webView.buttons["Continuar"].tap()
         }
-        
+        wait(for: app.buttons["button lend it"], timeout: 3.0)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // This method is called after the invocation of each test method in the class.
+        // Log out of app
+        app.navigationBars["Lend_It.MapView"].buttons["Item"].tap()
+        app.buttons["Configuración"].tap()
+        app.tables.staticTexts["Cerrar sesión"].tap()
     }
     
-    func testSignIn() {
-        Lend_ItUITests.app.buttons.element.tap()
-        //app.swipeLeft()
-        //app.buttons["Done"].tap()
+    func testNewItem() {
         
+        app.buttons["button lend it"].tap()
+        let scrollViewsQuery = app.scrollViews
+        scrollViewsQuery.otherElements.textFields["Ej. Martillo Cat"].tap()
+        app.keys["T"].tap()
+        app.keys["e"].tap()
+        app.keys["s"].tap()
+        app.keys["t"].tap()
+        
+        let element = scrollViewsQuery.children(matching: .other).element
+        element.children(matching: .other).element(boundBy: 0).children(matching: .other).element.children(matching: .button).matching(identifier: "Button").element(boundBy: 0).tap()
+        app.buttons["Cancel"].tap()
+        element.children(matching: .other).element(boundBy: 2).children(matching: .textField).element.tap()
+        app.keys["1"].tap()
+        app.keys["0"].tap()
+        app.keys["0"].tap()
+        element.tap()
+        app.navigationBars["Lend_It.AddItemView"].buttons["Cancel"].tap()
     }
 
+}
+
+
+extension XCTestCase {
+/// Wait for element to appear
+    func wait(for element: XCUIElement, timeout duration: TimeInterval) {
+        let predicate = NSPredicate(format: "exists == true")
+        let _ = expectation(for: predicate, evaluatedWith: element, handler: nil)
+        // We use a buffer here to avoid flakiness with Timer on CI
+        waitForExpectations(timeout: duration + 0.5)
+    }
 }
