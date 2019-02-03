06a9e0e7f21d8fcdda5b44d3269491bae384a614
Daniel Esteban Salinas Suárez <daniel@Daniels-MacBook-Pro.local>
2019-02-03 00:32:23 -0600

    Add some unit Tests

--- Lend ItTests/Lend_ItTests.swift
+++ Lend ItTests/Lend_ItTests.swift
@@ -10,25 +10,27 @@ import XCTest
 @testable import Lend_It
 
 class Lend_ItTests: XCTestCase {
+    
+    var mapView: MapViewController!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        let mapStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        mapView = mapStoryboard.instantiateViewController(withIdentifier: "MapViewController") as? MapViewController
+        mapView.loadView() // This line is the key
+
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testMapViewStartsHidden() {
+        XCTAssertFalse(mapView.hamburgerMenuIsVisible)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSearchBarText() {
+        XCTAssertEqual(mapView.searchBar?.placeholder, "¿Qué estás buscando?")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testLendButton() {
+        XCTAssertFalse(mapView.lendButton.isHidden)
     }
 
 }
